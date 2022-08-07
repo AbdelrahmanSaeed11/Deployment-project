7c34c51fe2ffee1b865786515403f6c01d209f6e
AbdelrahmanSaeed11 <abdelrahmansaeed2101@gmail.com>
2022-08-07 16:12:58 +0200

    update the connection links to backend

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'udagram-api',
-  apiHost: 'udagram-api-env.eba-kmksr9hy.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://udagram-api-env.eba-kmksr9hy.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
